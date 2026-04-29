a5223d01c797c41ca3fa51948372fb0df4150137
GANTA PRAVEEN <2400031145@kluniversity.in>
2026-04-29 18:58:00 +0530

    Update UserController.java

--- src/main/java/com/klef/fsad/sdp/controller/UserController.java
+++ src/main/java/com/klef/fsad/sdp/controller/UserController.java
@@ -1,14 +1,25 @@
 package com.klef.fsad.sdp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import com.klef.fsad.sdp.entity.User;
+import com.klef.fsad.sdp.service.UserService;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public String testUser() {
         return "User API Working 🔥";
     }
 
+    @PostMapping("/signup")
+    public String signup(@RequestBody User user) {
+        return userService.userRegistration(user);
+    }
 }
