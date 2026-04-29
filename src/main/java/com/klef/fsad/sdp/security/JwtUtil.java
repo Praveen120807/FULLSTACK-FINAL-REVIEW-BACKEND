cfd7082fb5c90800e37d7a756bfa4fe958d58784
GANTA PRAVEEN <2400031145@kluniversity.in>
2026-04-29 18:00:55 +0530

    Update JwtUtil.java

--- src/main/java/com/klef/fsad/sdp/security/JwtUtil.java
+++ src/main/java/com/klef/fsad/sdp/security/JwtUtil.java
@@ -1,51 +1,50 @@
 package com.klef.fsad.sdp.security;
 
-import java.security.Key;
 import java.util.Date;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import io.jsonwebtoken.*;
-import io.jsonwebtoken.security.Keys;
 
 @Component
-public class JwtUtil 
-{
-    private final String SECRET = "MySecretKeyForJWT12345678901234567890";
+public class JwtUtil {
 
-    private Key getKey()
-    {
-        return Keys.hmacShaKeyFor(SECRET.getBytes());
-    }
+```
+private String SECRET_KEY = "secret123";
 
-    public String generateToken(String username)
-    {
-        return Jwts.builder()
-                .setSubject(username)
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 8))
-                .signWith(getKey(), SignatureAlgorithm.HS256)
-                .compact();
-    }
+public String extractUsername(String token) {
+    return extractClaims(token).getSubject();
+}
 
-    public String extractUsername(String token)
-    {
-        return Jwts.parserBuilder()
-                .setSigningKey(getKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
-    }
+public Date extractExpiration(String token) {
+    return extractClaims(token).getExpiration();
+}
 
-    public boolean validateToken(String token)
-    {
-        return !Jwts.parserBuilder()
-                .setSigningKey(getKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getExpiration()
-                .before(new Date());
-    }
+private Claims extractClaims(String token) {
+    return Jwts.parser()
+            .setSigningKey(SECRET_KEY)
+            .parseClaimsJws(token)
+            .getBody();
+}
+
+private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+}
+
+public boolean validateToken(String token, UserDetails userDetails) {
+    String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+}
+
+public String generateToken(String username) {
+    return Jwts.builder()
+            .setSubject(username)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 5))
+            .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+            .compact();
+}
+```
+
 }
